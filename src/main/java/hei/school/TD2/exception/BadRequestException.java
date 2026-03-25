3ce9330e15ee5f82b0e6017042ba9862c12f536d
Koloina01 <kolomaherison@gmail.com>
2026-03-25 22:42:10 +0300

    feat: add BadRequestException for handling validation errors

---
+++ src/main/java/hei/school/TD2/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package hei.school.TD2.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
